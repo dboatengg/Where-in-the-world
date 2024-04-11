56d280f135d738dab2522518db428af5336c1569
dboatengg <dicksonboateng67@gmail.com>
2024-04-11 14:19:31 +0000

    create logo design

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ import SearchInput from "./SearchInput"
 const Navbar = () => {
   return (
     <Flex alignItems="center" gap="30px" height="80px" maxWidth="90%" mx="auto">
-        <Text>MovieHub</Text>
+        <Text fontWeight="bold" color="teal" fontSize={{base:'20px', sm:'25px', lg:'30px'}}>MovieHub</Text>
         <SearchInput/>
         <ColorSwitchMode/>
     </Flex>
