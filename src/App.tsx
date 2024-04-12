5fdbdf8ff9c021103d78d20fb5d9acb5a4139183
dboatengg <dicksonboateng67@gmail.com>
2024-04-12 07:09:13 +0000

    Setup project

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
-import { Grid, GridItem } from "@chakra-ui/react";
-import Navbar from "./components/Navbar";
+import { Heading } from "@chakra-ui/react";
 
 export default function App() {
 
   return (
-    <Grid templateAreas={{base:`"nav" "main"`, lg:`"nav nav" "sidebar main"`}} templateColumns={{base:"1fr" ,lg:"200px 1fr"}}>
-      <GridItem area="nav"><Navbar/></GridItem>
-      <GridItem area="sidebar" bg="coral" height="50rem"></GridItem>
-      <GridItem area="main" bg="red" height="50rem"></GridItem>
-    </Grid>
+    <Heading>Hello World</Heading>
   );
 }
