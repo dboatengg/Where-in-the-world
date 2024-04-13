131f05ab837cea856a06fbee7563856f591f4faf
dboatengg <dicksonboateng67@gmail.com>
2024-04-13 17:34:07 +0000

    fix width of skeletons

--- src/components/CardGrid.tsx
+++ src/components/CardGrid.tsx
@@ -29,7 +29,7 @@ const CardGrid = () => {
     <SimpleGrid spacing={10} columns={{base:1, md:2,lg:4}} paddingY="50px" maxWidth={{base:'90%',md:'95%'}} mx="auto">
         {loading && skeletons.map(skeleton=><Skeletons key={skeleton}/>)}
         {sortedData.map(country => (
-          <Flex key={country.name.common} flexDirection="column" boxShadow="xl" bg={colorMode === 'dark'?'gray.700':''} height="380px" borderRadius={5} maxWidth="350px">
+          <Flex key={country.name.common} flexDirection="column" boxShadow="lg" bg={colorMode === 'dark'?'gray.700':''} height="380px" borderRadius={5} maxWidth={{base:'100%',lg:'300px'}}>
             <Image height={{base:'250px', md:'200px'}} objectFit="cover" width="100%" src={country.flags[0]} borderTopLeftRadius={5} borderTopRightRadius={5}/>
             <Flex flexDirection="column" padding={5} alignItems="flex-start">
               <Link fontWeight="bold" _hover={{textDecoration:'none'}} fontSize="18px">{country.name.common}</Link>
--- src/components/Skeletons.tsx
+++ src/components/Skeletons.tsx
@@ -1,14 +1,15 @@
-import { Skeleton, SkeletonText, Stack } from '@chakra-ui/react'
+import { Card, Flex, Skeleton, SkeletonText, Stack } from '@chakra-ui/react'
 
 const Skeletons = () => {
   return (
-    <Stack maxWidth="350px">
+    <Card width={{base:'300px', sm:"220",lg:'250px'}} marginRight={{base:'',md:10}} boxShadow="lg">
         <Skeleton height="200px"/>
-        <Stack>
+        <Card padding="20px" height="180px">
           <SkeletonText mt='2' noOfLines={4} spacing='4' />
-        </Stack>
-    </Stack>
+        </Card>
+    </Card>
   )
 }
 
 export default Skeletons
+
