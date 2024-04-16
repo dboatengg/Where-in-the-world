e2fb61c8805a5e485c4a70d8d3e3da73e963d6c2
dboatengg <dicksonboateng67@gmail.com>
2024-04-16 11:07:46 +0000

    descrease font size of logo on smaller screens

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,7 +8,7 @@ const Navbar = () => {
     <Box boxShadow="md" bg={colorMode === 'dark'?'gray.700':''}>
         <Flex justifyContent="space-between" alignItems="center" height="60px" maxWidth="95%" mx="auto">
           <Link to="/">
-            <Text _hover={{ textDecoration: "none" }} ><Heading fontSize="18px" whiteSpace="nowrap">Where in the world?</Heading></Text>
+            <Text _hover={{ textDecoration: "none" }} ><Heading fontSize={{base:'16px', md:'18px'}} whiteSpace="nowrap">Where in the world?</Heading></Text>
           </Link>
             <SwitchMode/>
         </Flex>
